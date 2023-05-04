3807140fb6a94e28290ede4ed3dbc5807548447d
amclau303 <amclau303@caledonian.ac.uk>
2023-05-04 17:21:58 +0100

    auth

--- controllers/websiteController.js
+++ controllers/websiteController.js
@@ -9,3 +9,23 @@ exports.login_page = function (req, res) {
 exports.register_page = function (req, res) {
     res.render("register");
 }
+
+exports.post_new_user = function (req, res) {
+    const user = req.body.username;
+    const password = req.body.pass;
+
+    if (! user || !password) {
+        res.send(401, "No username/password");
+        return;
+    }
+
+    userDAO.lookup(user, function(err, u) {
+        if (u) {
+            res.send(401, "User exists", user);
+            return;
+        }
+        UserDAO.create(user, password);
+        console.log("Register user: ", user, "Password: ", password);
+        res.redirect("/login");
+    });
+}
--- routes/websiteRoutes.js
+++ routes/websiteRoutes.js
@@ -4,6 +4,8 @@ const controller = require("../controllers/websiteController.js");
 
 router.get("/", controller.index_page);
 router.get("/login", controller.login_page);
+
 router.get("/register", controller.register_page);
+router.post("/register", controller.post_new_user);
 
 module.exports = router;
