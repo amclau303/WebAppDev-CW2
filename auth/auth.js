5876b4389ddff9975b0bc10ca2995acf8d9ae054
amclau303 <amclau303@caledonian.ac.uk>
2023-05-04 17:32:28 +0100

    login addition & dotenv

--- auth/auth.js
+++ auth/auth.js
@@ -4,5 +4,29 @@ const jwt = require("jsonwebtoken");
 
 exports.login = function (req, res, next) {
 
-}
+    let username = req.body.username;
+    let password = req.body.password;
+
+    userModel.lookup(username, function (err, user) {
+        if(err) {
+            console.log("Error looking up user", err);
+            return res.status(401).send();
+            }
+        if (!user) {
+            console.log("User ", username, " not found");
+            return res.status(401).send();
+        }
+
+        //Compare provided password with stored password
+        bcrypt.compare(password, user.password, function (err, result) {
+            if (result) {
+                //if user exists we will write code to create the JWT here
+                //then pass onto the next middleware
+                next();
+            } else {
+                return res.status(403).send();
+            }
+        });
+    });
+};
 
--- routes/websiteRoutes.js
+++ routes/websiteRoutes.js
@@ -8,4 +8,6 @@ router.get("/login", controller.login_page);
 router.get("/register", controller.register_page);
 router.post("/register", controller.post_new_user);
 
+
+
 module.exports = router;
